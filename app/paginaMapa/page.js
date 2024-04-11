9e7bccba8e8452953f120e527a5edf030021f01e
Isabelleraf <57845159+Isabelleraf@users.noreply.github.com>
2024-04-11 02:03:11 -0300

    Create page.js

---
+++ app/paginaMapa/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Menu from "../components/menu";
+
+export default function Mapa(){
+    return (
+        <>
+            <Menu/>
+            <h1>Mapa</h1>
+            <Link href="/">Voltar</Link>
+        </>
+    );
+}
