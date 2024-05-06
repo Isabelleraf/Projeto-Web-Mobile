4609910429faf7274b5f3be48a78eb14eb8f304a
Isabelleraf <57845159+Isabelleraf@users.noreply.github.com>
2024-05-06 17:05:33 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
-import Menu from "./components/menu";
+import Link from "next/link";
+import Style from "./page.module.css";
 
-export default function Home() {
-  return (
-    <main>
-      <Menu />
-      <h1>Home</h1>
-    </main>
-  );
+
+export default function Quiz(){
+    return (
+        <>
+            <header className={Style.quiz}>
+                <h1>Quiz</h1>
+            </header>
+            
+            <Link href="/">Voltar</Link>
+        </>
+    );
 }
