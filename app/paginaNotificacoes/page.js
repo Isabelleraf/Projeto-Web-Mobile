1c8d0a24b1da6845dcf7c4a230607320ffd68071
Isabelleraf <57845159+Isabelleraf@users.noreply.github.com>
2024-05-15 18:59:06 -0300

    Update page.js

--- app/paginaNotificacoes/page.js
+++ app/paginaNotificacoes/page.js
@@ -1,17 +1,45 @@
+"use client"
+
 import Link from "next/link";
 import Style from "./page.module.css";
-
+import {useState, useEffect} from 'react'
 
 export default function Notificacoes(){
+    const [notificacoes, setNotificacoes] = useState(null);
+    
+    useEffect(() =>{
+        async function fetchNotificacoes() {
+            try {
+                const response = await fetch('/api/notificacao');
+                const data = await response.json();
+                setNotificacoes(data)
+            } catch (e) {
+                console.log(e)
+            }
+        }
+
+        fetchNotificacoes()
+    }, [])
+
     return (
         <>
+             
             <header className={Style.notificacoes}>
                 <h1>Notificações</h1>
             </header>
-            
-            <nav className={Style.voltar}>
-                <Link href="/">Voltar</Link>
+
+            <nav className={Style.botoes}>
+                <Link className={Style.voltar} href="/">Voltar</Link>
             </nav>
+            
+
+            {notificacoes && notificacoes.map((notificacao) => (
+                <div key={notificacao.id} className={Style.notificacao}>
+                    <strong>{notificacao.titulo}</strong>: {notificacao.descricao}
+                </div>
+            ))}
+
+           
         </>
     );
 }
