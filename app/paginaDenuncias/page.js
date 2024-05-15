840a888bcd5b2cc5d455c16b1a808917d5120418
Isabelleraf <57845159+Isabelleraf@users.noreply.github.com>
2024-05-15 18:51:16 -0300

    Update page.js

--- app/paginaDenuncias/page.js
+++ app/paginaDenuncias/page.js
@@ -1,14 +1,42 @@
+"use client"
+
 import Link from "next/link";
 import Style from "./page.module.css";
+import {useState, useEffect} from 'react'
 
 export default function Denuncias(){
+    const [denuncias, setDenuncias] = useState(false);
+    
+    useEffect(() =>{
+        async function fetchDenuncias() {
+            try {
+                const response = await fetch('/api/denuncia');
+                const data = await response.json();
+                setDenuncias(data)
+            } catch (e) {
+                console.log(e)
+            }
+        }
+
+        fetchDenuncias()
+    }, [])
+
     return (
         <>  
             <header className={Style.denuncias}>
                 <h1>Denúncias</h1>
             </header>  
+            <div className={Style.botoes}>
+                <Link className={Style.botaoVoltar} href="/">Voltar</Link>
+                <Link className={Style.botaoNovaDenuncia} href="/paginaNovaDenuncia">+</Link>
+            </div>
 
-            <Link href="/">Voltar</Link>
+            {denuncias && denuncias.map((denuncia) => (
+                <div key={denuncia.id} className={Style.denuncia}>
+                    <strong>{denuncia.local}</strong>: {denuncia.descricao}
+                </div>
+            ))}
+
         </>
     );
-}
+} 
