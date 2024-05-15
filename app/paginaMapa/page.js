6765b6932ee63b635d8d30de654612890457c54e
Isabelleraf <57845159+Isabelleraf@users.noreply.github.com>
2024-05-15 18:58:15 -0300

    Update page.js

--- app/paginaMapa/page.js
+++ app/paginaMapa/page.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Style from "./page.module.css";
+import Image from 'next/image'
+ 
 
 export default function Mapa(){
     return (
@@ -7,14 +9,29 @@ export default function Mapa(){
             <header className={Style.mapa}>
                 <h1>Mapa</h1>
             </header>
+
+            <nav className={Style.botoes}>
+                <Link className={Style.botaoVoltar} href="/">Voltar</Link>
+            </nav>
             
             <h2>Áreas de grande risco de acidentes:</h2>
-            
+            <div>
+                <Image
+                    src="/images/AreasDeRisco.png"
+                    width={500}
+                    height={300}
+                />
+            </div>
+
             <h2>Medidas de Segurança:</h2>
+            <div>
+                <Image
+                    src="/images/MedidasSeguranca.jpeg"
+                    width={300}
+                    height={300}
+                />
+            </div>
 
-                    
-
-            <Link href="/">Voltar</Link>
         </>
     );
 }
